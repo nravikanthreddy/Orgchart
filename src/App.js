a45f6194a326bba20186311a2d697e3ec930fe74
ravi Reddy <ravikanthreddy.n@gmail.com>
2024-03-11 12:09:43 -0700

    added auth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
-import { API, Storage } from 'aws-amplify';
+import { uploadData, getUrl, remove } from 'aws-amplify/storage';
 import {
     Button,
     Flex,
@@ -11,12 +11,21 @@ import {
     TextField,
     View,
     withAuthenticator,
-} from '@aws-amplify/ui-react';
+} from "@aws-amplify/ui-react";
 import { listNotes } from "./graphql/queries";
 import {
     createNote as createNoteMutation,
     deleteNote as deleteNoteMutation,
 } from "./graphql/mutations";
+
+// From ChatGPT:
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports'; // The path may vary
+import { generateClient } from 'aws-amplify/api';
+
+Amplify.configure(awsExports);
+
+const client = generateClient();
 
 const App = ({ signOut }) => {
     const [notes, setNotes] = useState([]);
@@ -26,15 +35,14 @@ const App = ({ signOut }) => {
     }, []);
 
     async function fetchNotes() {
-        const apiData = await API.graphql({ query: listNotes });
+        const apiData = await client.graphql({ query: listNotes });
         const notesFromAPI = apiData.data.listNotes.items;
         await Promise.all(
             notesFromAPI.map(async (note) => {
                 if (note.image) {
-                    const url = await Storage.get(note.name);
+                    const url = await getUrl({ key: note.id });
                     note.image = url;
-                }
-                return note;
+                } return note;
             })
         );
         setNotes(notesFromAPI);
@@ -47,27 +55,27 @@ const App = ({ signOut }) => {
         const data = {
             name: form.get("name"),
             description: form.get("description"),
-            image: image.name,
+            image: image.name
         };
-        if (!!data.image) await Storage.put(data.name, image);
-        await API.graphql({
+        const result = await client.graphql({
             query: createNoteMutation,
             variables: { input: data },
         });
+        if (!!data.image) await uploadData({ key: result.data.createNote.id, data: image }).result;
         fetchNotes();
         event.target.reset();
     }
 
-
     async function deleteNote({ id, name }) {
         const newNotes = notes.filter((note) => note.id !== id);
         setNotes(newNotes);
-        await Storage.remove(name);
-        await API.graphql({
+        await remove({ key: id });
+        await client.graphql({
             query: deleteNoteMutation,
             variables: { input: { id } },
         });
     }
+
     return (
         <View className="App">
             <Heading level={1}>My Notes App</Heading>
@@ -115,8 +123,8 @@ const App = ({ signOut }) => {
                         <Text as="span">{note.description}</Text>
                         {note.image && (
                             <Image
-                                src={note.image}
-                                alt={`visual aid for ${notes.name}`}
+                                src={note.image.url.href}
+                                alt={`visual aid for ${note.name}`}
                                 style={{ width: 400 }}
                             />
                         )}
